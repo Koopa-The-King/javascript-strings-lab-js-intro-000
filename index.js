f25eef954aa0dbec029df5873d55094db3acd8c5
Lucas Bowens <kingkoop4evr@gmail.com>
2017-10-16 22:18:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello Everybody!";
+var greeting = "Hello everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Neil deGrasse Tyson!" + specialGuest + "!";
+var greetSpecialGuest = "Neil deGrasse Tyson!"+"Hello," + specialGuest + "!";
 
 var topic = "space";
 
